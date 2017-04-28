3df0e5931fe5e1baafb3caed5e2dac67e55319ff
Daniel Baird <daniel@danielbaird.com>
2017-04-28 16:11:47 +1000

    difficult to describe pseudoprogress

--- data-preparation/matrix-making.py
+++ data-preparation/matrix-making.py
@@ -2,27 +2,51 @@
 
 # Rodeo commands to install packages
 
-! pip install pandas
-! pip install numpy
+# ! pip install pandas
+# ! pip install numpy
 
 # Import packages
 import numpy as np
 import pandas as pd
 
 # Make a dataframe from the rows containing each collaboration.  Each row has two authors in it
-df = pd.read_csv('collabs.csv', names=['last_name_1', 'first_name_1', 'orgu_1', 'publication_id', 'login_2', 'last_name_2', 'first_name_2', 'cat', 'orgu_2', 'organisation'])
+df1 = pd.read_csv('input/collabs.csv', usecols=['Family Name 1', 'Given Name 1', 'ORGU 1', 'Pub ID', 'Login 2', 'Family Name 2', 'Given Name 2', 'Cat', 'ORGU 2', 'Organisation'])
+df2 = pd.read_csv('input/collabs.csv', usecols=['Family Name 1', 'Given Name 1', 'ORGU 1', 'Pub ID', 'Login 2', 'Family Name 2', 'Given Name 2', 'Cat', 'ORGU 2', 'Organisation'])
 
-# series = df.groupby(['last_name_1','last_name_2']).size()
+df2['orig-fn1'] = df2['Family Name 1']
+df2['Family Name 1'] = df2['Family Name 2']
+df2['Family Name 2'] = df2['orig-fn1']
+
+df = pd.concat([df1, df2], ignore_index=True)
 
 #  Crosstab function
-ct = pd.crosstab(index=df['last_name_1'], columns = df['last_name_2'])
+ct = pd.crosstab(index=df['Family Name 1'], columns = df['Family Name 2'])
 
 # Write out to JSON without the indices/headers
 # Looks like the correct matrix as referenced in collab/graphs/*-matrix.json
-matrix_json = ct.to_json('matrix.json', orient = 'values')
+matrix_json = ct.to_json('James Cook University-graph-matrix.json', orient = 'values')
 
 # Get the authors in a list for the 'nodes.csv' file as required
-df_node = pd.read_csv('collabs.csv', names=['last_name_1', 'first_name_1', ])
+df_node = pd.read_csv('input/collabs.csv', usecols=['Family Name 1', 'Given Name 1', 'ORGU 1'])
+
+df_node['name'] = df_node['Given Name 1'] + df_node['Family Name 1']
+df_node['abbrv'] = df_node['Family Name 1']
+df_node['color'] = '#cc3333'
+# df_node['color'] = '#' + df_node['ORGU 1'].astype(str)
+df_node['coauthored'] = 1
+
+del df_node['Given Name 1']
+del df_node['Family Name 1']
+del df_node['ORGU 1']
+
+# another try at making df_node -------=-=---
+
+df_node = df[['Family Name 1']].copy()
+df_node['name'] = df_node['Family Name 1']
+df_node['abbrv'] = df_node['Family Name 1']
+df_node['color'] = '#cc3333'
+
+del df_node['Family Name 1']
 
 # Write out to CSV, similar with to_json just adding orient='values'. TODO: Needs to be amended to match
-nodes_csv = df_node.to_csv('nodes.csv', index = False)
+nodes_csv = df_node.to_csv('James Cook University-graph-nodes.csv', index = False)
