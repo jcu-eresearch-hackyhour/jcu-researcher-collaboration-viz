3ad303e9bad2498c0452572e28eeb4a93f0c6207
Daniel Baird <daniel@danielbaird.com>
2017-05-03 16:52:05 +1000

    adds collab to each researcher's collab list

--- data-preparation/dumber.py
+++ data-preparation/dumber.py
@@ -24,20 +24,26 @@ with open('input/testcollabs.csv', 'r') as csvfile:
 		# identify researcher at each end
 		res1 = researcher_id(pub['Given Name 1'], pub['Family Name 1'], pub['Login 1'])
 		res2 = researcher_id(pub['Given Name 2'], pub['Family Name 2'], pub['Login 2'])
+		collab_id = pub['Pub ID']
 
-		print(res1)
-		print(res2)
+		# pp.pprint(pub)
 
 		# make sure researcher is in researcher list
 		if res1 not in researchers:
-			researchers[res1] = { 'id': res1, 'collabs': [] }
+			researchers[res1] = { 'id': res1, 'collabs': {} }
 
 		if res2 not in researchers:
-			researchers[res2] = { 'id': res2, 'collabs': [] }
+			researchers[res2] = { 'id': res2, 'collabs': {} }
+
+		# add this line's collab to each researcher's collab list
+		researchers[res1]['collabs'][collab_id] = { 'id': collab_id, 'other': res2 }
+		researchers[res2]['collabs'][collab_id] = { 'id': collab_id, 'other': res1 }
+
+
+
 
 pp.pprint(researchers)
 
-#                add this line's collab to each researcher's collab list
 
 # produce final researcher list
 # produce grid of collabs (using same order of researchers as list)
