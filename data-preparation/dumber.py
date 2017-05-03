5704e493b53cac13a4af09964b5e63907642daf6
Daniel Baird <daniel@danielbaird.com>
2017-05-03 16:18:17 +1000

    pseudocode for dumber version

---
+++ data-preparation/dumber.py
@@ -0,0 +1,13 @@
+
+
+# start a list of researchers
+
+
+# read in each line of the pubs list
+
+# for each line: identify researcher at each end
+#                make sure researcher is in researcher list
+#                add this line's collab to each researcher's collab list
+
+# produce final researcher list
+# produce grid of collabs (using same order of researchers as list)
